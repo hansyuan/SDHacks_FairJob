4d5def6798eb04fe8c6595348cb3955998cb1bf3
hansyuan <hanzzeh@gmail.com>
2016-10-01 01:57:23 -0700

    added some NFc related stuff, shouldn't have changed any features (should only do hello world right now)

--- app/src/main/java/hansyuan/sdhacks_fairjob/MainActivity.java
+++ app/src/main/java/hansyuan/sdhacks_fairjob/MainActivity.java
@@ -1,13 +1,54 @@
 package hansyuan.sdhacks_fairjob;
 
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    NfcAdapter mNfcAdapter;
+    private FileUriCallback mFileUriCallback;
+
+    // List of URIs to provide to Android Beam
+    private Uri[] mFileUris = new Uri[10];
+
+    /**
+     * Callback that Android Beam file transfer calls to get
+     * files to share
+     */
+    private class FileUriCallback implements
+            NfcAdapter.CreateBeamUrisCallback {
+        public FileUriCallback() {
+        }
+        /**
+         * Create content URIs as needed to share with another device
+         */
+        @Override
+        public Uri[] createBeamUris(NfcEvent event) {
+            return mFileUris;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Android Beam file transfer is available, continue
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        /*
+         * Instantiate a new FileUriCallback to handle requests for
+         * URIs
+         */
+        mFileUriCallback = new FileUriCallback();
+        // Set the dynamic callback for URI requests.
+        mNfcAdapter.setBeamPushUrisCallback(mFileUriCallback,this);
+
+
+
+
+
     }
 }
